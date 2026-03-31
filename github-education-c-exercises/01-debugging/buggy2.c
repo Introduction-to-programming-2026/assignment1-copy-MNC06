a06f468a9d3481d007c03f90ce182528a86746bd
MNC06 <muhammed.nur.celil@ibu.edu.mk>
2026-03-31 18:16:12 +0000

    is it correct?

--- github-education-c-exercises/01-debugging/buggy2.c
+++ github-education-c-exercises/01-debugging/buggy2.c
@@ -1,8 +1,11 @@
-// Missing cs50.h, variable's type, semicolon, %s, and second printf argument.
+
 #include <stdio.h>
 
 int main(void)
 {
-    name = get_string("What's your name? ")
-    printf("hello, world\n");
+    char name[100];
+    printf("What's your name?");
+    scanf("%99s", name);
+    printf("hello, %s\n", name);
+    return 0;
 }
